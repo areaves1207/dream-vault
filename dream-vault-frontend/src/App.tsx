968d3cc1f5c623a5c74fc0cc57396c6fdd06961c
areaves1207 <areaves0816@gmail.com>
2025-10-27 16:21:37 -0600

    Updating UI

--- dream-vault-frontend/src/App.tsx
+++ dream-vault-frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Card from './components/DreamCard/DreamCard.tsx'
+import Header from './components/Header.tsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Header/>
+      <br></br>
       <Card title="Testing Title" description='Testing Description'></Card>
     </>
   )
