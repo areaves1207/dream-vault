c5e53968742b73972383764c6dd63ec0ee28bc23
areaves12 <areaves@mines.edu>
2025-11-03 06:39:11 +0100

    more database js updating

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2/promise');
+require('dotenv').config();
+const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'myapp',
+    database: 'dreamvault',
 });
 
-module.exports = pool;
+db.connect((err) =>{
+    if(err){
+        console.err("Error connecting to database: " + err.stack);
+        return;
+    }
+    console.log("Connected to MySQL successfully");
+});
+
+
