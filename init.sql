45590c818ab18681850374093334a3af98f5b359
areaves1207 <areaves0816@gmail.com>
2025-11-06 22:49:05 -0700

    Created database initializer

---
+++ init.sql
@@ -0,0 +1,14 @@
+--mysql -u root -p < init.sql
+CREATE DATABASE IF NOT EXISTS dreamvault;
+
+USE dreamvault;
+CREATE TABLE IF NOT EXISTS dreams(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title TEXT,
+    description TEXT,
+    date DATE
+);
+
+INSERT INTO dreams(title, description, date) VALUES ("TESTING 1", "DESCRIPTION 1", "2001-01-01");
+INSERT INTO dreams(title, description, date) VALUES ("TESTING 2", "DESCRIPTION 2", "2002-02-02");
+INSERT INTO dreams(title, description, date) VALUES ("TESTING 3", "DESCRIPTION 3", "2003-02-02");
