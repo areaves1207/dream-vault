fa8a997c2b9ce720c204c14e1e71bab2948ad623
areaves1207 <areaves0816@gmail.com>
2025-11-07 18:46:10 -0700

    Added users table and dummy data

--- init.sql
+++ init.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS dreams(
 INSERT INTO dreams(title, description, date) VALUES ("TESTING 1", "DESCRIPTION 1", "2001-01-01");
 INSERT INTO dreams(title, description, date) VALUES ("TESTING 2", "DESCRIPTION 2", "2002-02-02");
 INSERT INTO dreams(title, description, date) VALUES ("TESTING 3", "DESCRIPTION 3", "2003-02-02");
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255),
+    password TEXT,
+    created_at TIMESTAMP
+);
+
+INSERT INTO users(email, password) VALUES ("aidanemailtest@email.com", "unhashed_password_test");
