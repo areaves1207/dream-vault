5ed65b1adf364232c7d460f121d8aabfbd2091cd
areaves1207 <areaves0816@gmail.com>
2025-11-08 13:48:40 -0700

    Added routing in App

--- dream-vault-frontend/src/App.tsx
+++ dream-vault-frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return(
-    <>
-      <Home/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </Router>
   )
 }
 
