b37f3c683935f7f202a74038e1615625343edbcf
areaves1207 <areaves0816@gmail.com>
2025-11-11 16:08:33 -0700

    Routes now include state to give error messages

--- dream-vault-frontend/src/pages/Login.tsx
+++ dream-vault-frontend/src/pages/Login.tsx
@@ -1,10 +1,19 @@
 import style from "./Login.module.css"
 import InputForm from "../components/InputForm"
+import { useLocation } from "react-router-dom";
+
 
 export default function Login(){
+    const location = useLocation();
+    const message = location.state?.message
+
     return(
+        
         <div className={style.login}>
             <InputForm targeturl="http://localhost:3000/routes/login" titleText="Login to your dreamvault account"></InputForm>
+            {message && <div className={style.error}>
+                {message}
+            </div>}
         </div>
     )   
 }
--- dream-vault-frontend/src/pages/ProtectedRoute.tsx
+++ dream-vault-frontend/src/pages/ProtectedRoute.tsx
@@ -6,6 +6,7 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
     const token = document.cookie.includes("jwt=");
 
     if(!token){
+        console.error("NO JWT FOUND")
         return <Navigate to="/login" state={{ message: "Please log in to access this page." }} replace/>
     }
 
