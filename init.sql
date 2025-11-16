fcd66e04f0bdbe69ddd6c2a9da4a7b2e65ea73c1
areaves12 <areaves@mines.edu>
2025-11-16 21:05:58 +0100

    Added containerization for FRONTEND BACKEND and DB through Docker

--- init.sql
+++ init.sql
@@ -9,8 +9,6 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users(id, email, hashed_password) VALUES (-2,"test@email.com", "unhashed_password");
-
 CREATE TABLE IF NOT EXISTS dreams(
     dream_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     user_id INT NOT NULL,
@@ -19,7 +17,3 @@ CREATE TABLE IF NOT EXISTS dreams(
     date DATE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
-INSERT INTO dreams(user_id, title, description, date) VALUES (-2, "TESTING 1", "DESCRIPTION 1", "2001-01-01");
-INSERT INTO dreams(user_id, title, description, date) VALUES (-2, "TESTING 2", "DESCRIPTION 2", "2002-02-02");
-INSERT INTO dreams(user_id, title, description, date) VALUES (-2, "TESTING 3", "DESCRIPTION 3", "2003-02-02");
