0ca81ce213e3ce9869c469f2ac6431c33c92c443
areaves12 <areaves@mines.edu>
2025-11-22 00:35:08 +0100

    Fixed URL redirecting bug

--- dream-vault-frontend/src/pages/Login.tsx
+++ dream-vault-frontend/src/pages/Login.tsx
@@ -1,13 +1,13 @@
 import style from "./Login.module.css";
 import InputForm from "../components/InputForm";
 import { useLocation } from "react-router-dom";
-import { API_URL } from "../config.ts"
+import { FRONTEND_URL, API_URL } from "../config.ts"
 
 export default function Login() {
   const location = useLocation();
   const message = location.state?.message;
   const target_url = API_URL + "/routes/login";
-  const link_url = API_URL + "/register";
+  const link_url = FRONTEND_URL + "/register";
 
   return (
     <div className={style.login}>
