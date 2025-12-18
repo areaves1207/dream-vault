036faf8f1a2dc71f2eacbe9def74cb67c15c99d2
areaves12 <areaves@mines.edu>
2025-12-18 03:27:17 +0100

    Communication with backend reached

--- dream-vault-frontend/src/App.tsx
+++ dream-vault-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Home from './pages/Home';
 import Login from './pages/Login';
+import EmailVerification from './pages/VerifyEmail';
 import Register from "./pages/Register";
 import ProtectedRoute from "./pages/ProtectedRoute"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/verify-email" element={<EmailVerification/>}/>
 
         {/* Private routes. user must log in */}
         <Route path="/"           
